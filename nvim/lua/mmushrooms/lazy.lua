dbaba016e851f3c8dace3c21d04040661a24bcf8
Emilio Vera <72894753+jemveco@users.noreply.github.com>
2026-09-17 02:04:08 -0600

    Clean up lazy.lua by removing commented plugins
    Removed commented-out plugin configurations from lazy.lua.

--- nvim/lua/mmushrooms/lazy.lua
+++ nvim/lua/mmushrooms/lazy.lua
@@ -14,38 +14,4 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     spec = 'mmushrooms.lazy_plugs',
     change_detection = { notify = false },
-    --{
-    --    "folke/trouble.nvim",
-    --    dependencies = { "nvim-tree/nvim-web-devicons" },
-    --    opts = {
-    --        -- your configuration comes here
-    --        -- or leave it empty to use the default settings
-    --        -- refer to the configuration section below
-    --    },
-    --},
-    --{
-    --    'nvim-treesitter/nvim-treesitter',
-    --    build = ':TSUpdate',
-    --},
-    --{
-    --    'ThePrimeagen/harpoon',
-    --    branch = "harpoon2",
-    --    dependencies = { "nvim-lua/plenary.nvim" },
-    --},
-    ----'tpope/vim-fugitive',
-    --'nvim-treesitter/nvim-treesitter-context',
-    --'eandrju/cellular-automaton.nvim',
-    ---- For manage the Language Servers from neovim
-    --'williamboman/mason.nvim',
-    --'williamboman/mason-lspconfig.nvim',
-    --{
-    --    'VonHeikemen/lsp-zero.nvim',
-    --    branch = 'v3.x'
-    --},
-    ---- LSP support
-    --'neovim/nvim-lspconfig',
-    ---- Autocompletion
-    --'hrsh7th/cmp-nvim-lsp',
-    --'hrsh7th/nvim-cmp',
-    --'L3MON4D3/LuaSnip',
 })
