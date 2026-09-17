9edd375a1e3794c4f174b3ee2b1f3e6f7f43b644
Emilio Vera <72894753+jemveco@users.noreply.github.com>
2026-09-17 02:26:31 -0600

    Change telescope to use master branch and add key mapping
    Updated telescope configuration to use the master branch instead of a specific tag. Added key mapping for LSP document symbols.

--- nvim/lua/mmushrooms/lazy_plugs/telescope.lua
+++ nvim/lua/mmushrooms/lazy_plugs/telescope.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    -- tag = "0.1.5",
+    branch = "master",
     dependencies = { 'plenary' },
     name = 'telescope',
     priority = 1000,
@@ -10,6 +11,22 @@ return {
         })
 
         local builtin = require('telescope.builtin')
-
+        local actions = require("telescope.actions")
+        vim.keymap.set('n', '<leader>ds', function()
+            builtin.lsp_document_symbols({
+                symbols = {
+                    "Module", "Package", "Struct", "Class", "Property",
+                    "Field", "Constructor", "Method", "Enum", "Interface",
+                    "Function", "Constant", "Variable", "Number", "Boolean",
+                    "Array", "String", "Object", "Key", "EnumMember", "Null",
+                    "Event", "Operator", "TypeParameter",
+                },
+                symbol_width = 50,
+                attach_mappings = function(_, map)
+                    map("n", "q", actions.close)
+                    return true
+                end
+            })
+        end)
     end,
 }
